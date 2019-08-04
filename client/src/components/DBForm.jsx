879d404c35ef5ab374e409b57e086d0ec9bff815
luckysharms775 <arjun.sharma1229@gmail.com>
2019-08-04 13:06:04 -0500

    cleaned up the layout of the DBform and Fakeform

--- client/src/components/DBForm.jsx
+++ client/src/components/DBForm.jsx
@@ -2,18 +2,46 @@ import React from 'react';
 
 const DBForm = props => {
     return (
+       
+        
         <div>
+        <h4>  Product Description             
+        </h4>
+       
+       
+            <p> 
+                <input type='text' name='Brand_Name' placeholder='Brand_Name' onChange={props.change}></input><br></br> 
+            </p>
+
+            <p>
+                <input type='text' name='Pricing' placeholder='Pricing' onChange={props.change}></input><br></br>
+            </p>
+
+            <p>
+                <input type='text' name='In_Stock' placeholder='In_Stock' onChange={props.change}></input><br></br>
+            </p>
+
+            <p>
+                <input type='text' name='Item_weight' placeholder='Item_weight' onChange={props.change}></input><br></br>
+            </p>
+
+            <p> 
+                <input type='text' name='Size' placeholder='Size' onChange={props.change}></input><br></br>
+            </p>
+
+            <p>
+                <input type='text' name='Manufacturer_Number' placeholder='Manufacturer_Number' onChange={props.change}></input><br></br>
+            </p>
+    
+            <p> 
+                <button onClick={props.next}>next</button>
+            </p>
 
        
-        <input type='text' name='Brand_Name' placeholder='Brand_Name' onChange={props.change}></input><br></br>
-        <input type='text' name='Pricing' placeholder='Pricing' onChange={props.change}></input><br></br>
-        <input type='text' name='In_Stock' placeholder='In_Stock' onChange={props.change}></input><br></br>
-        <input type='text' name='Item_weight' placeholder='Item_weight' onChange={props.change}></input><br></br>
-        <input type='text' name='Size' placeholder='Size' onChange={props.change}></input><br></br>
-        <input type='text' name='Manufacturer_Number' placeholder='Manufacturer_Number' onChange={props.change}></input><br></br>
-        
-        <button onClick={props.next}>next</button>
+    
         </div>
+
+       
     )
 }
 
--- client/src/components/FakeForm.jsx
+++ client/src/components/FakeForm.jsx
@@ -3,12 +3,31 @@ import React from 'react';
 const FakeForm = props => {
     return (
         <div>
-        <input type='text' name='Product_Id' placeholder='Product_Id' onChange={props.change}></input><br></br>
-        <input type='text' name='Product_Name' placeholder='Product_Name' onChange={props.change}></input><br></br>
-        <input type='text' name='Product_Price' placeholder='Product_Price' onChange={props.change}></input><br></br>
-        <input type='text' name='Product_Description' placeholder='Product_Description' onChange={props.change}></input><br></br>
+
+        <h4>  
+        Product Description             
+        </h4>
+
+            <p> 
+                <input type='text' name='Product_Id' placeholder='Product_Id' onChange={props.change}></input><br></br>
+            </p>
         
-        <button onClick={props.back}>back</button>
+            <p> 
+                <input type='text' name='Product_Name' placeholder='Product_Name' onChange={props.change}></input><br></br>
+            </p>
+
+            <p> 
+                <input type='text' name='Product_Price' placeholder='Product_Price' onChange={props.change}></input><br></br>
+            </p>
+
+            <p> 
+                <input type='text' name='Product_Description' placeholder='Product_Description' onChange={props.change}></input><br></br>
+            </p> 
+
+            <p> 
+                <button onClick={props.back}>back</button>
+            </p> 
+
         </div>
     )
 }
