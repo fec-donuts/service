6475d9861127cf57d81ad3c1a8bafdf47f1e4ddf
Andrew Herrera <thinblueline2830@gmail.com>
2019-08-06 14:57:06 -0500

    Changed gallery to simiple css and html. Styled components with react was causing issues.

--- client/src/components/Photo.jsx
+++ client/src/components/Photo.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
+import ReactImageMagnify from 'react-image-magnify';
 const faker = require('faker');
 
  const Photo = (props) => {
  
-  const img = styled.img`
+  const Img = styled.img`
     display:flex;
     padding: 2rem 1rem;
     font-size: 1.2rem;
-    width: 150px;
-    height: 100px;
+    width: 350px;
+    height: 350px;
     float: screenLeft; 
   `;
   return (
-      <img src={faker.image.avatar()} alt="prop" />
+      // <Img src={faker.image.people()} alt="prop" />
+      <div></div>
   )
 
  };
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import styled from 'styled-components';
 //import ReactImageMagnify from 'react-image-magnify';
 import Photo from './components/Photo.jsx';
+import Gallery from './components/Gallery.jsx';
 
+
+// const Pg = styled.div`
+// display:flex;
+// /* padding: 2rem 1rem; */
+// /* font-size: 1.2rem; */
+// width: 50px;
+// height: 50px;
+// flex-direction: row;
+/* flex-wrap:  */
+/* align-items: flex-start; */
+/* justify-content: center; */
+/* align-content: flex-start;  */
+/* position: absolute; */
+// `;
 
 class App extends React.Component {
     constructor(props) {
@@ -27,12 +43,14 @@ class App extends React.Component {
         .catch(error => console.error(error))
       
     }
-  
+
+
+
     render() {
       return (
         <div>
           <h1>Hello from the other side.</h1>
-        <Photo photo={this.state.test}/>
+        <Photo />
         </div>
       );
     }
