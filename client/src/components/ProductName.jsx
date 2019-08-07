c8023f507f978196d3455143f3ba9488e2e725a0
Collin Fairlie <collin.fairlie@gmail.com>
2019-08-07 17:23:17 -0500

    Cleaned up some old code that wasn't being used, and converted RelatedItems from a class to a functional component.

--- client/src/components/ProductName.jsx
+++ client/src/components/ProductName.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-// import productNames from '../../../dataCreater.js';
+
 
 
 const ProductName = (props) => {
 
-    // productNames.map( name, index => {
+
         return (
             <a href="">{props.productName}</a>
         )
 
-    // })
+
 
 }
 
