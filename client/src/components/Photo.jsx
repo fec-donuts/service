1237b22ab113348fd9ac96ee8ce2e43e43f534dc
Collin Fairlie <collin.fairlie@gmail.com>
2019-08-08 14:35:18 -0500

    Changed for styled components to inline styling in order to pass over to the proxy server.

--- client/src/components/Photo.jsx
+++ client/src/components/Photo.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled, { keyframes } from 'styled-components';
+
 
 
 
@@ -9,16 +9,17 @@ const Photo = (props) => {
 
 
 
-    const Img = styled.img`
-        display: flex;
-        padding: 2rem 1rem;
-        font-size: 1.2rem;
-        width: 150px;
-        height: 100px;
-        float: left;
-    `;
+    const image = {
+        display: 'flex',
+        padding: '2rem 1rem',
+        width: '150px',
+        height: '100px',
+        float: 'left'
+
+    }
+    
         return (
-            <Img src={props.photo} alt="" ></Img>
+                <img style={image} src={props.photo} alt="" ></img>
         )
 
 }
